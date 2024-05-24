7929728caa8718e48b7060ac65e207cbab266d7f
sanjeev <mauryasanjeev@mooglelabs.com>
2024-05-24 11:17:54 +0530

    changes

--- dbconfig.js
+++ dbconfig.js
@@ -2,7 +2,8 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 dotenv.config();
 // connect with local mongoDB
-const dbHost = process.env.DATABSE_URL;
+// const dbHost = process.env.DATABSE_URL;
+const dbHost = "mongodb+srv://sanjumaurya7355248988:Jp2KhFSdT3EqkmNP@depin.gb1cezk.mongodb.net/"
 console.log(`DB Host: ${dbHost}`);
 mongoose.connect(dbHost);
 
